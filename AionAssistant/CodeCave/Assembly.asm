c330f3334a6a83853f06502676e4cd54f5df261d
Ayser Jamshidi <acejamshidi4th@gmail.com>
2022-07-16 06:39:03 -0400

    New, slimmed version with proof of concept code.

--- AionAssistant/CodeCave/Assembly.asm
+++ AionAssistant/CodeCave/Assembly.asm
@@ -1,52 +1,21 @@
-EXTERN jmpBackAddr : PROTO
+PUBLIC my_hook
+
+EXTERN jmpBackAddr:QWORD
+EXTERN entityAddr:QWORD
 EntityIntercept PROTO C
 
 .CODE
-PUBLIC AION
 
-AION PROC
-	; We push these or else we will be corrupting a specific
-	; part of the registers when we return to stack
-	pushfq
-	;push rax
-	;push rcx
-	;push rdx
-	;push rbx
-	push rbp
-	push rsi
-	;push rdi
-	;push r8
-	;push r9
-	;push r10
-	;push r11
-	;push r12
-	;push r13
-	;push r14
-	;push r15
+my_hook PROC
+	; Overwritten bytes
+	mov rax,[rax+20h]
+	add rsp,20h
 
-	call EntityIntercept
-	
-	;call writeHi
+	; Store RAX into entityAddr
+	mov [entityAddr],rax
 
-	; Place original registers
-	;pop r15
-	;pop r14
-	;pop r13
-	;pop r12
-	;pop r11
-	;pop r10
-	;pop r9
-	;pop r8
-	;pop rdi
-	pop rsi
-	pop rbp
-	;pop rbx
-	;pop rdx
-	;pop rcx
-	;pop rax
-	popfq
-
-	jmp qword ptr [jmpBackAddr]
-AION ENDP
+	; Jump back
+	jmp [jmpBackAddr]
+my_hook ENDP
 
 END
